ecb3d93eebef69da315b8679ca51964e0026afa1
Ozgur <ozgurkonsept3@gmail.com>
2026-01-28 16:32:08 +0300

    Remove explicit runtime=edge from pages to fix OpenNext build

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 
 import CategoryClient from "./CategoryClient"
 
-export const runtime = "edge"
+
 
 export default async function Page(props: { params: Promise<{ slug: string }> }) {
     const params = await props.params
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 
 import ProductClient from "./ProductClient"
 
-export const runtime = "edge"
+
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params
